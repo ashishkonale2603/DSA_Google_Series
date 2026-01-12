e893a764edfea8f602cd37c91128a2ab9e42b6ab
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-12 22:24:58 +0530

    new files for day 11-2

--- Chapter-1/3_max_subarray.py
+++ Chapter-1/3_max_subarray.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        
         maxSub=nums[0]
         curSum=0
         for n in nums:
