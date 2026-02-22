2b6f9098d8bb2c6d37b4403d236235f092161be4
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-22 22:54:08 +0530

    Correct indentation and formatting in Int_to_Roman.py
    Fix indentation and formatting issues in the code.

--- Chapter-1/Int_to_Roman.py
+++ Chapter-1/Int_to_Roman.py
@@ -4,7 +4,6 @@ def intToRoman(num):
         [100,'C'],[90,'XC'],[50,'L'],[40,'XL'],
         [10,'X'],[9,'IX'],[5,'V'],[4,'IV'],[1,'I']
            ]
-    
     result=''
     for i in range(len(store)):
         while num >= store[i][0]:
