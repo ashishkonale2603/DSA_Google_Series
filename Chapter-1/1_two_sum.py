c17b8550ef50bae493ce2e253ee592615143a98e
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-28 23:20:36 +0530

    Remove docstring type hints from twoSum method
    Removed type hints from the twoSum method docstring.

--- Chapter-1/1_two_sum.py
+++ Chapter-1/1_two_sum.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        
         prevMap = {}  # val: index
         for i, n in enumerate(nums):
             diff = target - n
