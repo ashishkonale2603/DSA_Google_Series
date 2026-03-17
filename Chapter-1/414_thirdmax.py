1ae997c5930fedbaccd476712bc69c9509936a88
ashishkonale2603 <ashishkonale@gmail.com>
2026-03-17 22:52:16 +0530

    sort

---
+++ Chapter-1/414_thirdmax.py
@@ -0,0 +1,13 @@
+class Solution():
+    def thirdmax(self,nums):
+
+        sortList=sorted(out)
+
+        if len(sortList)>=3:
+            return sortList[-3]
+        else:
+            return sortList[-1]
+
+lst=[2,2,3,1]
+s=Solution()
+print(s.thirdmax(lst))
